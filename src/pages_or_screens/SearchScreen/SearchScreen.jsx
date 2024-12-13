468d41222d5379f91033ad537ceb6275da177a52
vishilp <vishilthe3rd@gmail.com>
2024-12-13 02:10:40 -0800

    workign on google autocomplete

--- src/pages_or_screens/SearchScreen/SearchScreen.jsx
+++ src/pages_or_screens/SearchScreen/SearchScreen.jsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import {View, Text, TextInput, SafeAreaView} from "react-native";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
 
 import styles from "./styles";
 
 const SearchScreen = (props) => {
+    const [fromText, setFromText] = useState('')
+    const [destinationText, setDestinationText] = useState('')
+
     return(
         <SafeAreaView>
             <View style={styles.container}>
-                <TextInput style={styles.textInput} placeholder="From"/>
-                <TextInput style={styles.textInput}placeholder="Where to?"/>
+                <TextInput value = {fromText} onChangeText={setFromText}
+                style={styles.textInput} 
+                placeholder="From"/>
+                <TextInput value = {destinationText} onChangeText={setDestinationText}
+                style={styles.textInput}
+                placeholder="Where to?"/>
+
+                <GooglePlacesAutocomplete
+                    placeholder='Search'
+                    onPress={(data, details = null) => {
+                        // 'details' is provided when fetchDetails = true
+                        console.log(data, details);
+                    }}
+                    query={{
+                        key: 'Your API key',
+                        language: 'en',
+                    }}
+                    />
+
             </View>
         </SafeAreaView>
     )
--- src/pages_or_screens/SearchScreen/styles.jsx
+++ src/pages_or_screens/SearchScreen/styles.jsx
@@ -3,7 +3,8 @@ import { StyleSheet } from "react-native";
 const styles= StyleSheet.create(
     {
         container:{
-            padding:10
+            padding:10,
+            height: '100%'
         },
         textInput:{
             padding:15,
