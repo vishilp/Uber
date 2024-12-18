1d37922eec651b462db0c810bc52831cd6fb7862
vishilp <vishilthe3rd@gmail.com>
2024-12-17 21:51:34 -0800

    working on uber type row component

--- src/components/UberTypeRow/UberTypeRow.jsx
+++ src/components/UberTypeRow/UberTypeRow.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import {View, Text} from "react-native"
+import {View, Text, Image} from "react-native"
+import styles from "./styles";
+
+import Ionicons from "react-native-vector-icons/Ionicons"
 
 const UberTypeRow = () => {
     return(
-        <View>
-            <Text>placeholder for uber options</Text>
+        <View style= {styles.container}>
+            <Image style={styles.image} 
+            source={require("../../assets/Car_Images/UberX.jpeg")}/>
+
+            <View style= {styles.middleContainer}>
+                <Text style= {styles.vehicleType}>UberX
+                    <Ionicons name = {'person'} size = {12} />
+                    3
+                </Text>
+                <Text style={styles.time}>8:03PM dropoff</Text>
+                
+            </View>
+
+            <View style= {styles.rightContainer}>
+                <Ionicons name = {'pricetag'} size = {18} color = {'green'}/>
+                <Text style= {styles.price}>est. $27</Text>
+            </View>
         </View>
     )
 }
---
+++ src/components/UberTypeRow/styles.jsx
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create(
+    
+    {
+        container:{
+            flexDirection: 'row'
+        },
+
+        image:{
+            width: 30,
+            height: 30
+        },
+
+        middleContainer:
+        {
+
+        },
+
+        vehicleType:{
+
+        },
+
+        time: {
+
+        },
+
+        rightContainer: {
+
+        },
+
+        price: {
+
+        }
+    }
+)
+
+export default styles
