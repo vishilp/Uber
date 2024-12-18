c27aa5b960f16d359833ad414841a8adf03c9e6c
vishilp <vishilthe3rd@gmail.com>
2024-12-17 20:32:57 -0800

    initialized row components for order screen

---
+++ src/components/UberTypeRow/UberTypeRow.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text} from "react-native"
+
+const UberTypeRow = () => {
+    return(
+        <View>
+            <Text>placeholder for uber options</Text>
+        </View>
+    )
+}
+
+export default UberTypeRow
--- src/components/UberTypes/UberTypes.jsx
+++ src/components/UberTypes/UberTypes.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import {View, Text} from "react-native";
 import styles from "./styles";
-
+import UberTypeRow from "../UberTypeRow/UberTypeRow";
 
 const UberTypes = (props) => {
     return(
         <View>
-            
+            <UberTypeRow/>
         </View>
     )
 }
