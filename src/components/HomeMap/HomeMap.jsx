f0cacae2e351f25bbda48f13d3b44c735b5b4a35
vishilp <vishilthe3rd@gmail.com>
2024-12-19 13:45:40 -0800

    multiple cars rendering now

--- src/components/HomeMap/HomeMap.jsx
+++ src/components/HomeMap/HomeMap.jsx
@@ -1,26 +1,45 @@
 import React from "react";
-import {View, Text, Image} from "react-native";
-import  MapView, {Marker} from 'react-native-maps';
+import {View, Text, Image, FlatList} from "react-native";
+import  MapView, {Marker, MyCustomMarkerView} from 'react-native-maps';
+
+import cars from "../../assets/data/cars";
 
 const HomeMap = (props) => {
-    return(
 
-        <MapView style = {{height: '100%', width: '100%'}}
+    const getImage= (type) => {
+        if (type =='UberX')
+            return require("../../assets/Car_Images/top-UberX.png")
+        if (type =='Comfort')
+            return require("../../assets/Car_Images/top-Comfort.png")
+        return require("../../assets/Car_Images/top-UberXL.png")
+    }
+    
+    return(
+            <MapView
+            style = {{height: '100%', width: '100%'}}
             initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
+            latitude: 28.450627,
+            longitude: -16.263045,
+            latitudeDelta: 0.0222,
+            longitudeDelta: 0.0221,
             }}>
 
-            <Marker coordinate={{ latitude: 37.78825, longitude: -122.4324 }} />
+            {cars.map((car)=>(
+                <Marker key={car.id}
+                coordinate={{ latitude: car.latitude, longitude: car.longitude }}
+                tracksViewChanges={true}>
+                    
+                        <Image style= {{width: 30, height: 30, resizeMode: 'contain'}}
+                        source = {getImage(car.type)}/>
+    
+                </Marker>
+            ))}
+            
+            
 
-            <Marker coordinate={{latitude: 37.78825, longitude: -122.4324}}>
-                        <Image  style={{ width: 20, height: 20, resizeMode: 'contain' }}
-                        source={require('../../assets/Car_Images/top-UberX.png')}/>
-                    </Marker>
 
-        </MapView>
+            </MapView>
+            
     )
 }
 
