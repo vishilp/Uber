567d41e6afaec1401509062d86aded815c4ac118
vishilp <vishilthe3rd@gmail.com>
2024-12-18 21:59:24 -0800

    finished dummy uber types screen

--- src/components/UberTypes/UberTypes.jsx
+++ src/components/UberTypes/UberTypes.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-import {View, Text} from "react-native";
+import {View, Pressable, Text} from "react-native";
 import styles from "./styles";
 import UberTypeRow from "../UberTypeRow/UberTypeRow";
 
 import typesdata from '../../assets/data/types'
 
 const UberTypes = (props) => {
+
+    const confirm = () => {
+        console.warn('confirmed')
+    }
+
     return(
         <View>
             {typesdata.map(type => <UberTypeRow ubertype={type}/>)}
+
+            <Pressable onPress={confirm} style={styles.confirmButton}>
+                <Text style={styles.buttonText}>Confirm Uber</Text>
+            </Pressable>
+
         </View>
+
     )
 }
 
--- src/components/UberTypes/styles.jsx
+++ src/components/UberTypes/styles.jsx
@@ -3,7 +3,16 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create(
     
     {
-        
+        confirmButton:{
+            backgroundColor: 'black',
+            padding: 12,
+            margin: 10,
+            alignItems: 'center'
+        },
+        buttonText:{
+            color: 'white',
+            fontWeight:'bold'
+        }
     }
 )
 
