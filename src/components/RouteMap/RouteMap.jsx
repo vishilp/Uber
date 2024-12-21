4125571e1b0cc4e36363215d55229c36b7d63a2a
vishilp <vishilthe3rd@gmail.com>
2024-12-20 19:23:07 -0800

    render a route line for the uber

--- src/components/RouteMap/RouteMap.jsx
+++ src/components/RouteMap/RouteMap.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import {View, Text, Image, FlatList} from "react-native";
 import  MapView, {Marker, MyCustomMarkerView} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+import { GOOGLE_API_KEY } from '@env'
 
 import cars from "../../assets/data/cars";
 
@@ -12,20 +14,23 @@ const RouteMap = (props) => {
             longitudeDelta: 0.0221,
     });
 
-    const getImage= (type) => {
-        if (type =='UberX')
-            return require("../../assets/Car_Images/top-UberX.png")
-        if (type =='Comfort')
-            return require("../../assets/Car_Images/top-Comfort.png")
-        return require("../../assets/Car_Images/top-UberXL.png")
-    }
-    
+    const origin = {latitude: 28.450627, longitude: -16.263045}
+    const destination = {latitude: 28.450127, longitude: -16.269045}
+
     return(
             <MapView
             style = {{height: '100%', width: '100%'}}
             initialRegion={region}
             region={region}>
-            
+                
+                <MapViewDirections
+                origin={origin}
+                destination={destination}
+                apikey={GOOGLE_API_KEY}
+                strokeWidth = {3}
+                strokeColor = 'black'/>
+                <Marker coordinate={origin} title = {'origin'}/>
+                <Marker coordinate={destination} title = {'destination'}/>
             </MapView>
             
     )
