cc4f4f9b7857630f4c34d2c0deb8cfb13b607ac1
vishilp <vishilthe3rd@gmail.com>
2024-12-21 20:31:04 -0800

    app requests user location on startup

--- App.tsx
+++ App.tsx
@@ -5,7 +5,8 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { PermissionsAndroid } from 'react-native';
 import 'react-native-get-random-values';
 /* for the google places api*/
 
@@ -17,8 +18,40 @@ import OrderScreen from './src/pages_or_screens/OrderScreen/OrderScreen';
 
 
 function App(): React.JSX.Element {
+
+  {/*req permission for user location using useEffect (runs once automatically)*/}
+
+  const requestLocationPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Uber Location Permission',
+          message:
+            'Uber App needs access to your current location ' +
+            'so you can take awesome rides.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can now use your current location');
+      } else {
+        console.log('Location permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+
   return (
     <>
+
     <SearchScreen/>
     </>
       );
