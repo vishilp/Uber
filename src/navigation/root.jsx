4ddb8c6d18ce16a19bfa0f06f52263b9ea035a52
vishilp <vishilthe3rd@gmail.com>
2024-12-23 22:37:29 -0800

    updated navigation component

--- src/navigation/root.jsx
+++ src/navigation/root.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import {View, Text} from 'react-native'
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../pages_or_screens/HomeScreen/HomeScreen";
+import SearchScreen from "../pages_or_screens/SearchScreen/SearchScreen";
+import OrderScreen from "../pages_or_screens/OrderScreen/OrderScreen";
+
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 const RootNavigator = () => {
     return(
         <NavigationContainer>
-            <HomeScreen/>
+            <Stack.Navigator>
+                <Stack.Screen name = {"Home"} component={HomeScreen}/>
+                <Stack.Screen name = {"Search"} component={SearchScreen}/>
+                <Stack.Screen name = {"Order"} component={OrderScreen}/>
+            </Stack.Navigator>
         </NavigationContainer>
     )
 }
